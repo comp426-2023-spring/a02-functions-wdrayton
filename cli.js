59083633e83216c51dcff49dc81420b50c1565e9
wdrayton <wdrayton@live.unc.edu>
2023-02-23 19:51:38 -0500

    Third submission

--- cli.js
+++ cli.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-import { minimist } from 'minimist'
-import { moment } from 'moment-timezone'
-import { fetch } from 'node-fetch'
+import  minimist  from 'minimist'
+import  moment  from 'moment-timezone'
+import  fetch  from 'node-fetch'
 
 const args = minimist(process.argv.slice(2));
 
